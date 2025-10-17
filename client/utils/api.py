8da7ccf80b887da276537796294471beb16e054c
adithya95978 <adithyachinta2005@gmail.com>
2025-10-17 11:50:50 +0530

    Update api.py

--- client/utils/api.py
+++ client/utils/api.py
@@ -8,3 +8,9 @@ def upload_pdfs_api(files):
 
 def ask_question(question):
     return requests.post(f"{API_URL}/ask/",data={"question":question})
+
+def get_answer_with_image(question: str, image_file):
+    """Sends a question and an image to the backend."""
+    files_payload = {'image': (image_file.name, image_file.getvalue(), image_file.type)}
+    data_payload = {'question': question}
+    return requests.post(f"{API_URL}/ask_with_image", files=files_payload, data=data_payload)
